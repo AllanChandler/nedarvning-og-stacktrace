2fc15a41e4f8fb2df8975d1db566294fdb77fd52
AllanChandler <cph-ac326@cphbusiness.dk>
2023-10-12 10:44:43 +0200

    Task1

---
+++ Task1/src/Flows.java
@@ -0,0 +1,29 @@
+public class Flows {
+
+
+    public static void methodA(String input) {
+        System.out.println("j");
+        System.out.println("a");
+        System.out.println("v");
+        System.out.println("a");
+
+    }
+
+    public static void methodB(String input) {
+        System.out.println("e");
+        System.out.println("r");
+
+    }
+
+    public static void methodC(String input) {
+        System.out.println("s");
+        System.out.println("j");
+
+    }
+
+    public static void methodD(String input) {
+        System.out.println("o");
+        System.out.println("v");
+        System.out.println("t");
+    }
+}
---
+++ Task1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        Flows flows = new Flows();
+        flows.methodA("");
+        flows.methodB("");
+        flows.methodC("");
+        flows.methodD("");
+
+    }
+}
